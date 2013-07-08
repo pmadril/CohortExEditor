b43606ca17f3605e39ee582ae398c578a3bee55e
Dan Klco <dan.klco@sixdimensions.com>
2013-07-07 21:29:58 -0400

    Adding the code to edit a post

--- js/edit-post.js
+++ js/edit-post.js
@@ -1,4 +1,6 @@
 (function($) {
+	
+	var currentPost = null;
 
 	Stevenson.ext.afterInit(function() {
 		Stevenson.log.info('Editing post');
@@ -9,14 +11,14 @@
 		
 		if (Stevenson.Account.repo == '') {
 			Stevenson.log.warn('Website repository not set');
-			Stevenson.ui.Messages.displayError('Website repository not set.  <a href="account.html">Configure</a>');
+			Stevenson.ui.Messages.displayError('Website repository not set.  <a href="/account.html">Configure</a>');
 		}
 
 		var postPath = Stevenson.util.getParameter('post');
 
 		if(postPath == '' || postPath.indexOf('.html') != -1){
 			new nicEditor({
-				iconsPath : './img/nicEditorIcons.gif'
+				iconsPath : '/img/nicEditorIcons.gif'
 			}).panelInstance('content');
 		}
 		
@@ -28,6 +30,7 @@
 			Stevenson.repo.getFile({
 				path: postPath,
 				success: function(file){
+					currentPost = file;
 					Stevenson.log.debug('Retrieved post');
 
 					$('h2').append(postPath);
@@ -58,5 +61,57 @@
 	});
 	$(document).ready(function(){
 		$('#edit-post #date').datepicker();
+		$('.save').click(function(){
+			
+			Stevenson.ui.Loader.display('Saving post...', 100);
+			
+			var properties = currentPost.getProperties();
+			
+			var layout = $('#layout').val();
+			var title = $('#title').val();
+			if(properties) {
+				properties.layout = layout;
+				properties.title = title;
+			} else {
+				if(layout != '' || title != ''){
+					properties = {};
+					properties.layout = layout;
+					properties.title = title;
+				}
+			}
+			
+			var newContent = '';
+			if(properties){
+				Stevenson.log.debug('Adding Jekyll header');
+				newContent += '---\n';
+				newContent += YAML.stringify(properties);
+				newContent += '---\n\n';
+			} else {
+				Stevenson.log.debug('Not adding Jekyll header');
+			}
+			
+			var edit = nicEditors.findEditor('content');
+			if(edit) {
+				newContent += edit.getContent();
+			} else {
+				newContent += $('#content').val();
+			}
+			currentPage.content = newContent;
+			
+			Stevenson.repo.savePage({
+				page: currentPost,
+				path: Stevenson.util.getParameter('post'),
+				message: $('#message').val(),
+				error: function(message){
+					Stevenson.ui.Loader.hide();
+					Stevenson.ui.Messages.displayError('Exception saving post: '
+							+ message);
+				},
+				success: function(){
+					Stevenson.ui.Loader.hide();
+					window.location = '/edit-site.html#' + Stevenson.util.getParameter('path');
+				}
+			});
+		});
 	});
 })(jQuery);
